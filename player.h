8611a06cff498566458a00b4f5ccf6eb15108393
Rdhoel <romgavrilko@gmail.com>
2013-12-30 00:43:23 +0200

    Create player.h

---
+++ player.h
@@ -0,0 +1,34 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#pragma once
+
+#include <hge.h>
+#include <hgevector.h>
+#include <hgeanim.h>
+
+#define PLAYER_FRICTION 0.95
+
+class player
+{
+private:
+	static HGE*		hge;
+	hgeVector		Position;
+	hgeVector		Velocity;
+	HTEXTURE		Texture;
+	hgeAnimation*	Sprite;
+	hgeRect			BoundingBox;
+	float			Speed;
+public:
+	player(hgeVector Position, hgeVector Velocity/*,HTEXTURE &Texture*/);
+	~player();
+
+	bool Update(float delta);
+	void Render();
+	void		ReverseVelocity() { Velocity = hgeVector(-Velocity.x,-Velocity.y); };
+	void		SetVelocity(hgeVector velocity) { Velocity = velocity; };
+	void		SetPosition(hgeVector position) { Position = position; };
+	hgeRect		GetBoundingBox() { return BoundingBox; };
+	hgeVector	GetPosition() { return Position; };
+};
+
+#endif
